c5dfcb3db24c67436017b5e1815a44dae2bc4fac
ilhemboubaker <ilhem.boubaker580@gmail.com>
2023-12-11 17:40:47 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './counter/Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>Counter </h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
